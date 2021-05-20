d60783195b23eb33e5dd0a25deffb9ae6ca17ea9
Zachary <duvallzt@gmail.com>
2021-05-20 16:21:00 -0600

    Add function to render html

--- music_controller/frontend/views.py
+++ music_controller/frontend/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Allows us to rener index.html and then react takes care of the rest
+def index(req, *args, **kwargs):
+    return render(req, 'frontend/index.html')
