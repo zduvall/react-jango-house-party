99ac7c4a7d4587b6ff906d6c18cb5dff92c183ac
Zachary <duvallzt@gmail.com>
2021-05-20 18:08:33 -0600

    Import Homepage component

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,8 +1,17 @@
 import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
 
+// components
+import HomePage from './HomePage';
+
 export default function App() {
-  return <h1>Testing React Code</h1>;
+  return (
+    // <h1>Test</h1>
+    <div>
+      <h1>Testing</h1>
+      <HomePage />
+    </div>
+  );
 }
 
 render(
